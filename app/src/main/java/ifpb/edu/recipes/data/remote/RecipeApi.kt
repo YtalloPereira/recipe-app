a5691865e7285fb3cee83b54f4a2fc69d46cd218
YtalloPereira <ytallopereiralves@gmail.com>
2025-03-13 14:33:07 -0300

    :sparkles: feat: adicionando interface da api e instancia do retrofit

---
+++ app/src/main/java/ifpb/edu/recipes/data/remote/RecipeApi.kt
@@ -0,0 +1,9 @@
+package ifpb.edu.recipes.data.remote
+
+import ifpb.edu.recipes.data.model.RecipeResponse
+import retrofit2.http.GET
+
+interface RecipeApi {
+    @GET("recipes/random?number=10&apiKey=4f285b25f9df4505a2e5b63dce3d8fab")
+    suspend fun getRecipes(): RecipeResponse
+}
