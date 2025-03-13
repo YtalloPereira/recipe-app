c554389668957335f478420f85a4c2fd77e5ac2f
YtalloPereira <ytallopereiralves@gmail.com>
2025-03-13 14:31:53 -0300

    :sparkles: feat: adicionando classes do model,recipe e recipe response]

---
+++ app/src/main/java/ifpb/edu/recipes/data/model/Recipe.kt
@@ -0,0 +1,3 @@
+package ifpb.edu.recipes.data.model
+
+data class Recipe(val id: Int, val title: String, val image: String)
---
+++ app/src/main/java/ifpb/edu/recipes/data/model/RecipeResponse.kt
@@ -0,0 +1,3 @@
+package ifpb.edu.recipes.data.model
+
+data class RecipeResponse(val recipes: List<Recipe>)
