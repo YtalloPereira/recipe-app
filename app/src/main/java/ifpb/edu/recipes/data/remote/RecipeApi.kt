d20cc73336b6e644d9b9ca30aba413c7be428833
YtalloPereira <ytallopereiralves@gmail.com>
2025-03-14 14:32:36 -0300

    :sparkles: feat: adicionando rota para obetenção das instruções de uma receita

--- app/src/main/java/ifpb/edu/recipes/data/remote/RecipeApi.kt
+++ app/src/main/java/ifpb/edu/recipes/data/remote/RecipeApi.kt
@@ -1,5 +1,6 @@
 package ifpb.edu.recipes.data.remote
 
+import ifpb.edu.recipes.data.model.AnalyzedInstruction
 import ifpb.edu.recipes.data.model.IngredientResponse
 import ifpb.edu.recipes.data.model.RecipeResponse
 import retrofit2.http.GET
@@ -11,4 +12,7 @@ interface RecipeApi {
 
     @GET("recipes/{id}/ingredientWidget.json?apiKey=4f285b25f9df4505a2e5b63dce3d8fab")
     suspend fun getIngredients(@Path("id") recipeId: Int): IngredientResponse
+
+    @GET("recipes/{id}/analyzedInstructions?apiKey=4f285b25f9df4505a2e5b63dce3d8fab")
+    suspend fun getInstructions(@Path("id") recipeId: Int): List<AnalyzedInstruction>
 }
