0006007577bc720cf96d31b2d1830060f9573384
YtalloPereira <ytallopereiralves@gmail.com>
2025-03-18 12:44:31 -0300

    :broom: cleanup: removendo código comentado

--- app/src/main/java/ifpb/edu/recipes/MainActivity.kt
+++ app/src/main/java/ifpb/edu/recipes/MainActivity.kt
@@ -13,37 +13,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-/**
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RecipesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipesTheme {
-        Greeting("Android")
-    }
-}
-        **/
